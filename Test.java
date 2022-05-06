c8f7c45e9c7220fcbc575232800f98973bbcad0f
Ilir Hajdari <ilir.hajdari111@gmail.com>
2022-05-06 00:30:45 -0400

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,16 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		Manager mgr = new Manager();
+		mgr.setFirstName("Anakin");
+		mgr.setLastName("Skywalker");
+		mgr.setEmployeeID(501);
+		mgr.setSalary(10000);
+		mgr.setDepartment("Command");
+		mgr.EmployeeSummary();
+		
+	}
+	
+}
